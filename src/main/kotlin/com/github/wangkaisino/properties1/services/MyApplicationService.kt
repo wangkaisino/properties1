b574f76f31b7d652ae80573eda89d8c6a355564c
GitHub Action <action@github.com>
2022-09-05 03:08:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangkaisino/properties1/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wangkaisino/properties1/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangkaisino.properties1.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangkaisino.properties1.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/wangkaisino/properties1/MyPluginTest.kt
+++ src/test/kotlin/com/github/wangkaisino/properties1/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wangkaisino.properties1
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
